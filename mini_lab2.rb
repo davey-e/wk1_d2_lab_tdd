fe3c51daa968544798569112f0305dc39f02982b
Dave Ellis <dave@ellisnetmail.co.uk>
2017-11-21 21:31:50 +0000

    Cleaning up mini_lab2.rb

--- mini_lab2.rb
+++ mini_lab2.rb
@@ -1,39 +1,10 @@
-# [:MINI LAB]
-# >
-# > 1. Create an add function
-# >   - this will take two parameters
-# >   - call the first parameter first_number and the second second_number
-# >   - use the return keyword
-# >   - Invoke the function `add( 2, 3 )`
-# > 2. Create a population_density function
-# >   - this will take two parameters
-# >   - call the first parameter population and the second area
-# >   - calculate the population density (e.g. `population / area`) and return it
-# >   - invoke the function using the population and area of Mauritius:
-# >     - population: 5373000
-# >     - area: 77933
-# >
-
 def add(first_number, second_number)
   return first_number + second_number
 end
 
-value1 = 2
-value2 = 3
-p add(value1, value2)
-
-def population_density(population, area)
-  return population / area
-end
-
-population = 5373000
-area = 77933
-p population_density(population, area)
-
 def subtract(first_number, second_number)
   return first_number - second_number
 end
-
 
 def multiply(first_number, second_number)
   return first_number * second_number
